77c608ad5db43683234fcb740a036c4748daa2c4
SaboorHaider <saboorzaidi5@gmail.com>
2024-06-20 19:44:41 +0500

    ATM MACHINE

---
+++ index.ts
@@ -0,0 +1,68 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+let myBalance = 15000;
+let myPin = 555;
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+            name: "pin",
+            message:"enter your pin",
+            type: "number",
+        }
+    ]
+);
+
+if(pinAnswer.pin === myPin){
+    console.log(chalk.green("correct pin code"));
+
+    let operations = await inquirer.prompt(
+        [
+            {
+                name: "operation",
+                message:"set operation",
+                type: "list",
+                choices: ["withdraw", "checkbalance", "fastCash"],
+            }
+        ]
+    );
+    if(operations.operation === "withdraw"){
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message:"enter your amount",
+                    type: "number",
+                }
+            ]
+        );
+        
+      if(amountAns.amount > myBalance) {
+         console.log(chalk.red("Insufficient Balance"))
+    }else if (myBalance -= amountAns.amount) {
+        console.log(chalk.green(`your remaining amount is: ${myBalance}`));
+     }
+    };
+    if(operations.operation === "checkbalance") {
+        console.log(`your balance is:  ${myBalance}`);
+    }
+   if (operations.operation === "fastCash") {
+    let cash = await inquirer.prompt(
+        [
+            {
+                name: "options",
+                message: "select any amount",
+                type: "list",
+                choices: ["500","1000", "2000", "5000"],
+            }
+        ]
+    );
+    if (myBalance -= cash.options) {
+        console.log(chalk.green(`your remaining amount is: ${myBalance}`));
+    }
+   }
+}else {
+    console.log(chalk.redBright("Incorrect pin code"));
+}
